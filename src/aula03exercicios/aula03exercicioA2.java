eda7ecb6a45e1032073b940462b9d3eb45bf465a
ice <ice@lab06m14.nrc.ice.ufjf.br>
2017-08-15 22:21:49 -0300

    Exercício A realizado

---
+++ src/aula03exercicios/aula03exercicioA2.java
@@ -0,0 +1,18 @@
+
+package aula03exercicios;
+
+import javax.swing.JFrame;
+
+public class aula03exercicioA2 {
+
+   public static void main(String[] args) {
+       
+       aula03exercicioA janela = new aula03exercicioA();
+       janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       janela.setSize(250,250);
+       janela.setLocationRelativeTo(null);
+       janela.setVisible(true);
+       
+   }
+    
+}
